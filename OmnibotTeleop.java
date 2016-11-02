6461af9dfb2bbbedfd5c1527bcb28217dcbe9225
Aftershock9880 <ftc.9880@gmail.com>
2016-11-02 15:20:52 -0700

    Commented out spinner

--- OmnibotTeleop.java
+++ OmnibotTeleop.java
@@ -46,7 +46,7 @@ public class OmnibotTeleop extends OpMode {
     DcMotor motorFr;
     DcMotor motorBl;
     DcMotor motorBr;
-	DcMotor spinner;
+	//commented ohhhDcMotor spinner;
 
 	/**
 	 * Constructor
