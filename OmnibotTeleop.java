a777d088601607ef5842d8a4a98a8284d48062e8
Aftershock9880 <ftc.9880@gmail.com>
2016-11-03 17:29:55 -0700

    Does not work on #4

--- OmnibotTeleop.java
+++ OmnibotTeleop.java
@@ -102,15 +102,18 @@ public class OmnibotTeleop extends OpMode {
 
 		// note that if y equal -1 then joystick is pushed all of the way forward.
 
-		Flpower = -gamepad1.left_stick_y + gamepad1.left_stick_x;
-		Frpower = -gamepad1.left_stick_y + -gamepad1.left_stick_x;
-		Blpower = -gamepad1.left_stick_y + -gamepad1.left_stick_x;
-		Brpower = -gamepad1.left_stick_y + gamepad1.left_stick_x;
-
-		Flpower = gamepad1.right_stick_x;
-		Frpower = -gamepad1.right_stick_x;
-		Blpower = gamepad1.right_stick_x;
-		Brpower = -gamepad1.right_stick_x;
+		//if (-gamepad1.right_stick_x < gamepad1.right_stick_x|| gamepad1.right_stick_x > 0.01) {
+		//	Flpower = gamepad1.right_stick_x;
+		//	Frpower = -gamepad1.right_stick_x;
+		//	Blpower = gamepad1.right_stick_x;
+		//	Brpower = -gamepad1.right_stick_x;
+		//}
+		//else {
+		//	Flpower = -gamepad1.left_stick_y + gamepad1.left_stick_x;
+		//	Frpower = -gamepad1.left_stick_y + -gamepad1.left_stick_x;
+		//	Blpower = -gamepad1.left_stick_y + -gamepad1.left_stick_x;
+		//	Brpower = -gamepad1.left_stick_y + gamepad1.left_stick_x;
+		//}
 
 		motorFl.setPower(Flpower);
 		motorFr.setPower(Frpower);
@@ -156,39 +159,5 @@ public class OmnibotTeleop extends OpMode {
 	@Override
 	public void stop() {
 
-	}
-
-	/*
-	 * This method scales the joystick input so for low joystick values, the
-	 * scaled value is less than linear.  This is to make it easier to drive
-	 * the robot more precisely at slower speeds.
-	 */
-	double scaleInput(double dVal)  {
-		double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
-				0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
-
-		// get the corresponding index for the scaleInput array.
-		int index = (int) (dVal * 16.0);
-
-		// index should be positive.
-		if (index < 0) {
-			index = -index;
-		}
-
-		// index cannot exceed size of array minus 1.
-		if (index > 16) {
-			index = 16;
-		}
-
-		// get value from the array.
-		double dScale = 0;
-		if (dVal < 0) {
-			dScale = -scaleArray[index];
-		} else {
-			dScale = scaleArray[index];
-		}
-
-		// return scaled value.
-		return dScale;
 	}
 }
