8e9e187339176347baeb566507172c845e4541ab
Aftershock9880 <ftc.9880@gmail.com>
2016-12-16 17:20:16 -0800

    removed unused comments

--- TestRigTeleOp.java
+++ TestRigTeleOp.java
@@ -39,20 +39,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @TeleOp(name="Test Rig Teleop", group="TestRig")  // @Autonomous(...) is the other common choice
 //@Disabled
 public class TestRigTeleOp extends OpMode
