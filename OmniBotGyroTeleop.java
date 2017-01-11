3359e8e7fac8d4a1c503e09a7cf29e2e5d12c2db
student <ftc.9880@gmail.com>
2017-01-10 16:23:18 -0800

    added PD gyro control

--- OmniBotGyroTeleop.java
+++ OmniBotGyroTeleop.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import static com.qualcomm.robotcore.util.Range.clip;
+
 @TeleOp(name = "OmniBot Gyro Teleop", group = "OmniBot")
 //@Disabled
 public class OmniBotGyroTeleop extends OpMode {
@@ -12,6 +14,22 @@ public class OmniBotGyroTeleop extends OpMode {
 	private double powerDivider = 1; //Divide power by this much
 
     int direction;
+    double lastError = 0;
+
+    double Flpower;
+    double Frpower;
+    double Blpower;
+    double Brpower;
+
+    double kp = 1/600;
+    //double ki = 1/600;
+    double kd = 1/600;
+
+    double p = 0;
+    //double i = 0;
+    double d = 0;
+
+    double finalPD = 0;
 
 	@Override
 	public void init() {
@@ -39,16 +57,46 @@ public class OmniBotGyroTeleop extends OpMode {
 			powerDivider = 1;
 		}
 
-        robot.motorFl.setPower(-gamepad1.left_stick_y +  gamepad1.left_stick_x +  gamepad1.right_stick_x);
-        robot.motorFr.setPower(-gamepad1.left_stick_y + -gamepad1.left_stick_x + -gamepad1.right_stick_x);
-        robot.motorBl.setPower(-gamepad1.left_stick_y + -gamepad1.left_stick_x +  gamepad1.right_stick_x);
-        robot.motorBr.setPower(-gamepad1.left_stick_y +  gamepad1.left_stick_x + -gamepad1.right_stick_x);
+        if (gamepad1.right_stick_x > 0.001 || gamepad1.right_stick_x < -0.001) {
+            Flpower = -gamepad1.left_stick_y +  gamepad1.left_stick_x +  gamepad1.right_stick_x;
+            Frpower = -gamepad1.left_stick_y + -gamepad1.left_stick_x + -gamepad1.right_stick_x;
+            Blpower = -gamepad1.left_stick_y + -gamepad1.left_stick_x +  gamepad1.right_stick_x;
+            Brpower = -gamepad1.left_stick_y +  gamepad1.left_stick_x + -gamepad1.right_stick_x;
+            direction = robot.gyro.getHeading();
+        }
+        else {
+            Flpower = -gamepad1.left_stick_y +  gamepad1.left_stick_x;
+            Frpower = -gamepad1.left_stick_y + -gamepad1.left_stick_x;
+            Blpower = -gamepad1.left_stick_y + -gamepad1.left_stick_x;
+            Brpower = -gamepad1.left_stick_y +  gamepad1.left_stick_x;
+        }
+
+        Flpower = clip(Flpower, -0.7, 0.7);
+        Frpower = clip(Frpower, -0.7, 0.7);
+        Blpower = clip(Blpower, -0.7, 0.7);
+        Brpower = clip(Brpower, -0.7, 0.7);
 
         if (gamepad1.right_stick_x > 0.001 || gamepad1.right_stick_x < -0.001) {
                 direction = robot.gyro.getHeading();
         }
 
-        while (robot.gyro.getHeading() < direction) {
+        p = robot.gyro.getHeading() - direction;
+        //i =+ p;
+        d = p - lastError;
+        lastError = p;
+        double finalPD = p * kp + d * kd;
+
+        Flpower =+  finalPD;
+        Frpower =+ -finalPD;
+        Blpower =+  finalPD;
+        Brpower =+ -finalPD;
+
+        robot.motorFl.setPower(Flpower);
+        robot.motorFr.setPower(Frpower);
+        robot.motorBl.setPower(Blpower);
+        robot.motorBr.setPower(Brpower);
+
+        /*while (robot.gyro.getHeading() < direction) {
             robot.motorFl.setPower(0.1);
             robot.motorFr.setPower(-0.1);
             robot.motorBl.setPower(0.1);
@@ -59,7 +107,7 @@ public class OmniBotGyroTeleop extends OpMode {
             robot.motorFr.setPower(0.1);
             robot.motorBl.setPower(-0.1);
             robot.motorBr.setPower(0.1);
-        }
+        }*/
 
         /*/sweeper code, Gamepad 1 controls sweeping in with right bumper
 		if(gamepad1.right_bumper){
@@ -85,14 +133,17 @@ public class OmniBotGyroTeleop extends OpMode {
 
         //launcher code, Gamepad controls the conveyor and launcher wheels with b
         if(gamepad2.b) {
-			//robot.launcher.setPower(1);
-            //robot.conveyor.setPower(1);
+			robot.launcher.setPower(1);
+            robot.conveyor.setPower(1);
 		}
 
 		//Send telemetry data back to driver station.
-		telemetry.addData("stick X: ", -gamepad1.left_stick_x);
-		telemetry.addData("stick Y: ", -gamepad1.left_stick_y);
+		telemetry.addData("left stick X: ", -gamepad1.left_stick_x);
+		telemetry.addData("left stick Y: ", -gamepad1.left_stick_y);
+        telemetry.addData("right stick Y: ", -gamepad1.right_stick_y);
 		telemetry.addData("power divider: ", powerDivider);
         telemetry.addData("gyro heading: ", robot.gyro.getHeading());
+
+        telemetry.addData("PD loop addition: ", finalPD);
 	}
 }
